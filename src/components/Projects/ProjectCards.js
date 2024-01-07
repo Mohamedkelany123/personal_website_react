35453136a1aae475c5f7754061e9fd807a0f66f4
Mohamedkelany123 <mohamedkelany479@gmail.com>
2024-01-07 16:40:38 +0200

    Modified Projects Section To Take Summary + Technologies

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,3 +1,4 @@
+// Updated ProjectCards component with improved button layout
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
@@ -9,32 +10,54 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
+        <Card.Title style={{ color: '#c160e6'}}><b>{props.title}</b></Card.Title>
+        
+        {/* Displaying Summary */}
+        <Card.Text style={{ textAlign: "justify", color: "white" }}>
+         {props.description.summary}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
-        {!props.isBlog && props.demoLink && (
+         {/* Displaying Technologies as bullet points */}
+         {props.description.technologies && (
+          <Card.Text style={{ textAlign: "justify", color: "white" }}>
+            <strong style={{color: '#c160e6'}}>Technologies:</strong>
+            <ul>
+              {props.description.technologies.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+          </Card.Text>
+        )}
+        {/* <Card.Text style={{ textAlign: "justify" }}>
+          {props.description}
+        </Card.Text> */}
+
+        {/* GitHub buttons */}
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
           <Button
             variant="primary"
-            href={props.demoLink}
+            href={props.ghLinks[0]}
             target="_blank"
-            style={{ marginLeft: "10px" }}
+            style={{ marginBottom: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <BsGithub /> &nbsp; {props.ghNames[0] || "GitHub"}
           </Button>
-        )}
+          {props.ghLinks.slice(1).map((link, index) => (
+            <Button
+              key={`github_link_${index}`}
+              variant="primary"
+              href={link}
+              target="_blank"
+              style={{ marginLeft: "10px", marginBottom: "10px" }}
+            >
+              <BsGithub /> &nbsp; {props.ghNames[index + 1] || `GitHub ${index + 2}`}
+            </Button>
+          ))}
+        </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
