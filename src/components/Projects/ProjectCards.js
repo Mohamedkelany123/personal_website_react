c4b873d058e64ff310e8e44cadbce059d2459b9d
Mohamedkelany123 <mohamedkelany479@gmail.com>
2024-01-20 16:30:12 +0200

    Modified projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,8 +1,6 @@
-// Updated ProjectCards component with improved button layout
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
@@ -10,38 +8,30 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{ color: '#c160e6'}}><b>{props.title}</b></Card.Title>
-        
-        {/* Displaying Summary */}
-        <Card.Text style={{ textAlign: "justify", color: "white" }}>
-         {props.description.summary}
-        </Card.Text>
+        <Card.Title style={{ color: "#c160e6" }}>
+          <b>{props.title}</b>
+        </Card.Title>
 
-
-         {/* Displaying Technologies as bullet points */}
-         {props.description.technologies && (
-          <Card.Text style={{ textAlign: "justify", color: "white" }}>
-            <strong style={{color: '#c160e6'}}>Technologies:</strong>
-            <ul>
-              {props.description.technologies.map((tech, index) => (
-                <li key={index}>{tech}</li>
-              ))}
-            </ul>
-          </Card.Text>
-        )}
-
+        {/* Displaying sections dynamically */}
+        {props.sections &&
+          props.sections.map((section, index) => (
+            <div key={index} style={{textAlign: 'left'}}>
+              <strong  style={{ color: '#c160e6'}}>{section.title}:</strong>
+              {Array.isArray(section.text) ? (
+                <ul>
+                  {section.text.map((item, i) => (
+                    <li key={i}>{item}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>{section.text}</p>
+              )}
+            </div>
+          ))}
 
         {/* GitHub buttons */}
         <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-          <Button
-            variant="primary"
-            href={props.ghLinks[0]}
-            target="_blank"
-            style={{ marginBottom: "10px" }}
-          >
-            <BsGithub /> &nbsp; {props.ghNames[0] || "GitHub"}
-          </Button>
-          {props.ghLinks.slice(1).map((link, index) => (
+          {props.ghLinks.map((link, index) => (
             <Button
               key={`github_link_${index}`}
               variant="primary"
@@ -49,7 +39,7 @@ function ProjectCards(props) {
               target="_blank"
               style={{ marginLeft: "10px", marginBottom: "10px" }}
             >
-              <BsGithub /> &nbsp; {props.ghNames[index + 1] || `GitHub ${index + 2}`}
+              <BsGithub /> &nbsp; {props.ghNames[index] || `GitHub ${index + 1}`}
             </Button>
           ))}
         </div>
